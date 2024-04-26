69bbe29bfc548734972e44b765d1f61bd0965118
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-04-26 10:17:03 +0300

    add

---
+++ js/sign,log.js
@@ -0,0 +1,58 @@
+var username=document.getElementById('username');
+var email=document.getElementById('email');
+var password=document.getElementById('password');
+var form1=document.getElementById('form1');
+
+var userDataFromStorage = JSON.parse(localStorage.getItem('Data'));
+
+var alluser = userDataFromStorage || [];
+
+form1.addEventListener('submit',function(e){
+    e.preventDefault()
+
+
+    var userData = {
+        username: username.value,
+        email: email.value,
+        password: password.value,  
+    };
+
+    alluser.push(userData);
+    localStorage.setItem('Data', JSON.stringify(alluser));
+
+
+
+});
+
+
+// Login
+
+var formLogin = document.getElementById('formlogin'); 
+var emailLogin = document.getElementById('emaillogin');
+var passwordLogin = document.getElementById('passwordlogin');
+
+formLogin.addEventListener('submit', function(event) { 
+  event.preventDefault(); 
+
+  var allUsers = JSON.parse(localStorage.getItem('Data')); 
+
+  if (!allUsers) { 
+    alert('No user data found in localStorage.');
+    return; 
+  }
+
+  for (var i = 0; i < allUsers.length; i++) {
+    if (allUsers[i].email === emailLogin.value && allUsers[i].password === passwordLogin.value) {
+      console.log('Login successful!');
+      localStorage.setItem('loginUser', JSON.stringify(allUsers[i]));
+      window.location.href = 'index.html';
+      return; 
+    }
+  }
+
+  console.log('Invalid email or password.');
+  alert('Invalid email or password.');
+});
+
+
+
