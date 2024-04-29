68f73a7922f20779d9d7a15483569d1f03f2cade
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-04-29 11:02:58 +0300

    add

--- js/index.js
+++ js/index.js
@@ -1,3 +1,70 @@
+var number = document.getElementById('number');
+
+function shop(index) {
+  switch (index) {
+    case 1:
+        number.innerHTML = parseFloat(number.innerHTML) + 1.00;
+      break;
+    case 2:
+        number.innerHTML = parseFloat(number.innerHTML) + 6.00;
+      break;
+    case 3:
+        number.innerHTML = parseFloat(number.innerHTML) + 2.00;
+      break;
+    case 4:
+      number.innerHTML= parseFloat(number.innerHTML) + 1.00;
+      break;
+    case 5:
+        number.innerHTML = parseFloat(number.innerHTML) + 2.00;
+      break;
+    case 6:
+        number.innerHTML = parseFloat(number.innerHTML) + 6.00;
+      break;
+    case 7:
+        number.innerHTML = parseFloat(number.innerHTML) + 2.10;
+      break;
+    case 8:
+        number.innerHTML = parseFloat(number.innerHTML) + 2.00;
+      break;
+    case 9:
+        number.innerHTML = parseFloat(number.value) + 2.75;
+      break;
+    case 10:
+        number.innerHTML = parseFloat(number.value) + 2.50;
+      break;
+    case 11:
+        number.innerHTML = parseFloat(number.value) + 2.00;
+      break;
+    case 12:
+        number.innerHTML = parseFloat(number.value) + 2.00;
+      break;
+    case 13:
+        number.innerHTML= parseFloat(number.value) + 6.00;
+      break;
+    case 14:
+        number.innerHTML= parseFloat(number.value) + 3.00;
+      break;
+    case 15:
+        number.innerHTML = parseFloat(number.value) + 5.00;
+      break;
+    case 16:
+        number.innerHTML = parseFloat(number.value) + 1.00;
+      break;
+    case 17:
+        number.innerHTML = parseFloat(number.value) + 3.00;
+      break;
+    case 18:
+        number.innerHTML = parseFloat(number.value) + 2.00;
+      break;
+    default:
+      // Do nothing if the index is not valid
+      break;
+  }
+}
+
+
+
+
 
 function home(){
 
@@ -10,22 +77,6 @@ function login(){
     window.location.href='sign,log.html';
 }
 
-
-function shop(index){
-    if(index==1){
-        alert('index:1')
-    }
-
-  else if(index==2){
-        alert('index:2')
-    }
-    else if(index==3){
-        alert('index:3')
-    }
-    else if(index==4){
-        alert('index:4')
-    }
-}
 
 
 
