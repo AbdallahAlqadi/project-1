ad8840795648ba9fe23b64762d7eb7649a6b9a06
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-05-10 15:28:39 +0300

    add

--- js/index.js
+++ js/index.js
@@ -116,148 +116,148 @@ function addCard() {
 //strar
 
 //t1
-var t1 = document.createElement('div');
+var t1 = document.createElement('span');
     t1.className = 'card-t1';
    t1.innerHTML = 'Barbecue_salad : ';
     var n1= document.createElement('a');
    n1.className = 'primary1';
-   n1.innerHTML = item1;
+   n1.innerHTML = item1+`</br>`;
 
    //t2
-   var t2 = document.createElement('div');
+   var t2 = document.createElement('span');
     t2.className = 'card-t2';
    t2.innerHTML = 'Salad_with_fish : ';
     var n2= document.createElement('a');
    n2.className = 'primary2';
-   n2.innerHTML = item2;
+   n2.innerHTML = item2+`</br>`;
 
    //t3
-   var t3 = document.createElement('div');
+   var t3 = document.createElement('span');
     t3.className = 'card-t3';
    t3.innerHTML = 'Spinach_salad : ';
     var n3= document.createElement('a');
    n3.className = 'primary3';
-   n3.innerHTML = item3;
+   n3.innerHTML = item3+`</br>`;
 
    //t4
-   var t4 = document.createElement('div');
+   var t4 = document.createElement('span');
     t4.className = 'card-t4';
    t4.innerHTML = 'Salamon_Salad : ';
     var n4= document.createElement('a');
    n4.className = 'primary4';
-   n4.innerHTML = item4;
+   n4.innerHTML = item4+`</br>`;
 
    //t5
-   var t5 = document.createElement('div');
+   var t5 = document.createElement('span');
     t5.className = 'card-t5';
    t5.innerHTML = 'Pasta: ';
     var n5= document.createElement('a');
    n5.className = 'primary5';
-   n5.innerHTML = item5;
+   n5.innerHTML = item5+`</br>`;
 
    //t6
-   var t6 = document.createElement('div');
+   var t6 = document.createElement('span');
     t6.className = 'card-t6';
    t6.innerHTML = 'Grilled_chicken : ';
     var n6= document.createElement('a');
    n6.className = 'primary6';
-   n6.innerHTML = item6;
+   n6.innerHTML = item6+`</br>`;
 
    //t7
-   var t7 = document.createElement('div');
+   var t7 = document.createElement('span');
     t7.className = 'card-t7';
    t7.innerHTML = 'Pancakes : ';
     var n7= document.createElement('a');
    n7.className = 'primary7';
-   n7.innerHTML = item7;
+   n7.innerHTML = item7+`</br>`;
 
 //t8
-   var t8 = document.createElement('div');
+   var t8 = document.createElement('span');
     t8.className = 'card-t8';
    t8.innerHTML = 'Fruit_Salad : ';
     var n8= document.createElement('a');
    n8.className = 'primary8';
-   n8.innerHTML = item8;
+   n8.innerHTML = item8+`</br>`;
 //t9
-   var t9 = document.createElement('div');
+   var t9 = document.createElement('span');
     t9.className = 'card-t9';
    t9.innerHTML = 'Stack : ';
     var n9= document.createElement('a');
    n9.className = 'primary9';
-   n9.innerHTML = item9;
+   n9.innerHTML = item9+`</br>`;
 
    //t10
-   var t10 = document.createElement('div');
+   var t10 = document.createElement('span');
     t10.className = 'card-t10';
    t10.innerHTML = 'Strawberry_Juice : ';
     var n10= document.createElement('a');
    n10.className = 'primary10';
-   n10.innerHTML = item10;
+   n10.innerHTML = item10+`</br>`;
 
    //t11
-   var t11 = document.createElement('div');
+   var t11 = document.createElement('span');
     t11.className = 'card-t11';
    t11.innerHTML = 'Orange_Juice : ';
     var n11= document.createElement('a');
    n11.className = 'primary11';
-   n11.innerHTML = item11;
+   n11.innerHTML = item11+`</br>`;
 
    //t12
-   var t12 = document.createElement('div');
+   var t12 = document.createElement('span');
     t12.className = 'card-t12';
    t12.innerHTML = 'Mango_Juice : ';
     var n12= document.createElement('a');
    n12.className = 'primary12';
-   n12.innerHTML = item12;
+   n12.innerHTML = item12+`</br>`;
 
    //13
-   var t13 = document.createElement('div');
+   var t13 = document.createElement('span');
     t13.className = 'card-t13';
    t13.innerHTML = 'Pineapple_Juice : ';
     var n13= document.createElement('a');
    n13.className = 'primary13';
-   n13.innerHTML = item13;
+   n13.innerHTML = item13+`</br>`;
 
    
 //t14
-   var t14 = document.createElement('div');
+   var t14 = document.createElement('span');
     t14.className = 'card-t14';
    t14.innerHTML = 'Lemon_Juice : ';
     var n14= document.createElement('a');
    n14.className = 'primary14';
-   n14.innerHTML = item14;
+   n14.innerHTML = item14+`</br>`;
 
 //t15
-   var t15 = document.createElement('div');
+   var t15 = document.createElement('span');
     t15.className = 'card-t15';
    t15.innerHTML = 'Watermelon_Juice : ';
     var n15= document.createElement('a');
    n15.className = 'primary15';
-   n15.innerHTML = item15;
+   n15.innerHTML = item15+`</br>`;
 
 //t16
-   var t16 = document.createElement('div');
+   var t16 = document.createElement('span');
     t16.className = 'card-t16';
    t16.innerHTML = 'Guava_Juice : ';
     var n16= document.createElement('a');
    n16.className = 'primary16';
-   n16.innerHTML = item16;
+   n16.innerHTML = item16+`</br>`;
 
 //t17
-   var t17 = document.createElement('div');
+   var t17 = document.createElement('span');
     t17.className = 'card-t17';
    t17.innerHTML = 'Cocktail_Juice: ';
     var n17= document.createElement('a');
    n17.className = 'primary17';
-   n17.innerHTML = item17;
+   n17.innerHTML = item17+`</br>`;
 
 //t18
-   var t18 = document.createElement('div');
+   var t18 = document.createElement('span');
     t18.className = 'card-t18';
    t18.innerHTML = 'Avocado_Juice : ';
     var n18= document.createElement('a');
    n18.className = 'primary18';
-   n18.innerHTML = item18;
+   n18.innerHTML = item18+`</br>`;
 
 
    
@@ -271,13 +271,15 @@ var t1 = document.createElement('div');
 
     var total = document.createElement('a');
     total.className = 'primary';
-    total.innerHTML = (number.innerHTML+' JD');
+    total.innerHTML = (number.innerHTML+' JD'+`</br>`);
 
 
     var contact = document.createElement('span');
     contact.className = 'card-body';
+
     contact.append(tagTotal);
     contact.append(total);
+   
     //srat
     contact.append(t1);
     contact.append(n1);
